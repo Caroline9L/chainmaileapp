23e05fcc531a28762af40d51df693fe6fb6e9239
Caro <Caroline9L@users.noreply.github.com>
2017-10-09 11:51:29 -0400

    Add files via upload
    Updated form, added shared component and weave model

---
+++ src/app/shared/weave.model.ts
@@ -0,0 +1,17 @@
+export class Weave {
+ public name: string;
+ public imagePath: string;
+ public url: string;
+ public ringGauge: string;
+ public ringSize: string;
+
+
+ constructor(name: string, imagePath: string, url: string, ringGauge: string, ringSize: string) {
+   this.name = name;
+   this.imagePath = imagePath;
+   this.url = url;
+   this.ringGauge = ringGauge;
+   this.ringSize = ringSize;
+ }
+} 
+
--- src/app/suggest/suggest.component.ts
+++ src/app/suggest/suggest.component.ts
@@ -1,49 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-suggest',
   templateUrl: './suggest.component.html',
   styleUrls: ['./suggest.component.css']
 })
-export class SuggestComponent implements OnInit {
-  suggestionForm: FormGroup;
-
-  ngOnInit() {
-    this.suggestionForm = new FormGroup({
-      'weaveData': new FormGroup({
-        'weaveName': new FormControl(null),
-        'weavePhoto': new FormControl(null),
-        'tutorial': new FormControl(null),
-        'ringSizes': new FormArray([])
-      })
-    });
-
-    this.suggestionForm.statusChanges.subscribe(
-      (status) => console.log(status)
-    );
-
-    this.suggestionForm.setValue({
-      'weaveData': {
-        'weaveName': 'European 4-in-1',
-        'weavePhoto': 'www.test.com',
-        'tutorial': 'www.test.com',
-        'ringSizes': []
-      }
-    });
-
+export class SuggestComponent {
+  @ViewChild('f') suggestionForm: NgForm;
+  weave = {
+    weaveName: '',
+    weavePhoto: '',
+    tutorial: '',
+    ringGauge: '',
+    ringSize: ''
   }
+  submitted = false;
+
+  //function to convert the decimal to a fraction
 
   onSubmit() {
     console.log(this.suggestionForm);
+    this.submitted = true;
+    this.weave.weaveName = this.suggestionForm.value.weaveName;
+    this.weave.weavePhoto = this.suggestionForm.value.weavePhoto;
+    this.weave.tutorial = this.suggestionForm.value.tutorial;
+    this.weave.ringGauge = this.suggestionForm.value.ringGauge;
+    this.weave.ringSize = this.suggestionForm.value.ringSize;
     this.suggestionForm.reset();
-  }
-
-  onAddRing() {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.suggestionForm.get('ringSizes')).push(control)
   }
 
 }
