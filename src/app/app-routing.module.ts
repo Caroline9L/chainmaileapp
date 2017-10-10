6185d5d3ee88104511ef3f462a7e61f1049a3b24
Caro <Caroline9L@users.noreply.github.com>
2017-10-10 16:52:46 -0400

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
+import { WeaveItemComponent } from './browse-weaves/weave-list/weave-item/weave-item.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { SuggestComponent } from './suggest/suggest.component';
+import { BrowseWeavesComponent } from './browse-weaves/browse-weaves.component';
 
 
 const appRoutes: Routes = [
 	{ path: '', component: HomeComponent, pathMatch: 'full' },
-	{ path: 'suggest', component: SuggestComponent }
+	{ path: 'suggest', component: SuggestComponent },
+	{ path: 'browse-weaves', component: BrowseWeavesComponent, children: [
+		{ path: ':id', component: WeaveItemComponent }
+	] }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +8,11 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SuggestComponent } from './suggest/suggest.component';
+import { BrowseWeavesComponent } from './browse-weaves/browse-weaves.component';
+import { WeaveListComponent } from './browse-weaves/weave-list/weave-list.component';
+import { WeaveItemComponent } from './browse-weaves/weave-list/weave-item/weave-item.component';
+import { WeaveService } from './shared/weave.service';
+import { WeaveDetailComponent } from './browse-weaves/weave-detail/weave-detail.component';
 
 
 @NgModule({
@@ -17,13 +22,17 @@ import { SuggestComponent } from './suggest/suggest.component';
     HomeComponent,
     FooterComponent,
     SuggestComponent,
+    BrowseWeavesComponent,
+    WeaveListComponent,
+    WeaveItemComponent,
+    WeaveDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    FormsModule
   ],
-  providers: [],
+  providers: [WeaveService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
